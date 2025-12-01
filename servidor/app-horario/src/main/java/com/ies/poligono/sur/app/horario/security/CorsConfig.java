d2cf12b4bb7a556044d6e9d02d31e5133630dfbe
JoseMi-1222 <josearras343@gmail.com>
2025-12-01 18:52:00 +0100

    feat: Add additional allowed origin to CORS configuration

--- servidor/app-horario/src/main/java/com/ies/poligono/sur/app/horario/security/CorsConfig.java
+++ servidor/app-horario/src/main/java/com/ies/poligono/sur/app/horario/security/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173", "http://172.17.0.2:5173"));
+        config.setAllowedOrigins(List.of("http://localhost:5173", "http://172.17.0.2:5173", "http://localhost:5147"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
